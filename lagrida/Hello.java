aa65b7d2158885517cdd3387aa336eb6e112f189
Lagrida <lagyassine@gmail.com>
2021-08-26 11:33:52 +0100

    a commit tikchbila

--- lagrida/Hello.java
+++ lagrida/Hello.java
@@ -3,7 +3,7 @@ package lagrida;
 public class Hello {
 
 	public static void main(String[] args) {
-		int max = 10;
+		int max = 22;
 		int k = 0;
 		for(k=0; k < max; k++){
 			System.out.println("Hello World : " + k);
