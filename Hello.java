fb5622578844a67af408323774381f5d858e573b
Lagrida <lagyassine@gmail.com>
2021-08-26 10:32:43 +0100

    first commit

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		int max = 10;
+		int k = 0;
+		for(k=0; k < max; k++){
+			System.out.println("Hello World : " + k);
+		}
+	}
+}
